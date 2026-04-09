0659831d7968624cfe6bf5164e2f27c4d6f29896
arc <wagwag0224@gmail.com>
2026-04-09 11:34:50 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "CardSystem"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,23 @@
+package org.gang
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true // JSON에 클래스에 없는 필드가 있어도 무시
+        })
+    }
+
+    configureDatabases()
+}
+
